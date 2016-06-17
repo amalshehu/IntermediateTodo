c44538936b53be08104c995e2bafb74ace5743f0
amal_shehu <amalshehu@gmail.com>
2016-06-17 06:23:12 +0000

    Authentication and registration Routed

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+// Authentication Routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration Routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
