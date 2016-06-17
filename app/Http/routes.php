f0863dc4f690f2260ac439df5b40ea4a1cfc0772
amal_shehu <amalshehu@gmail.com>
2016-06-17 09:46:26 +0000

    Authentication scaffolding (generated-php artisan make:auth)

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -22,3 +22,7 @@ Route::get('auth/logout', 'Auth\AuthController@getLogout');
 // Registration Routes...
 Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
