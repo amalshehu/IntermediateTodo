10008ed3bb55285834564f4d4482bb22ef04d324
amal_shehu <amalshehu@gmail.com>
2016-06-17 05:39:58 +0000

    Task Controller modified

--- app/Task.php
+++ app/Task.php
@@ -2,14 +2,23 @@
 
 namespace App;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
     /**
-    * The attributes that are mass assignable.
-    *
-    * @var array
-    */
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = ['name'];
+
+    /**
+     * Get the user that owns the task.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
