6ec48a96a3e88076138b2f902aea29a987dc9d5c
amal_shehu <amalshehu@gmail.com>
2016-06-17 04:26:40 +0000

    Added Intermediate Laravel TO-DO App

---
+++ database/migrations/2016_06_16_094717_create_tasks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->index(); //Added according to tutorial.
+            $table->string('name');              //Added according to tutorial.
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tasks');
+    }
+}
